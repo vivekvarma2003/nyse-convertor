efe7934a8041fa9c0cae0ae4fe1878f7db9da922
vivekvarma2003 <vv674717@gmail.com>
2024-09-10 15:57:59 +0530

    added environment variables

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
 from dask import dataframe as dd
+import os
 columns = ['ticker', 'trade_date', 'open_price', 'low_price', 'high_price', 'close_price', 'volume']
 
 
 def convert_file_to_json():
+    src_dir = os.environ.setdefault('SRC_DIR','data/nyse_all/nyse_data')
+    tgt_dir = os.environ.setdefault('TGT_DIR','data/nyse_all/nyse_json')
     df = dd.read_csv(
-        'data/nyse_all/nyse_data/*',
+        f'{src_dir}/*',
         names=columns,
         blocksize=None
         )   
     df.to_json(
-        'data/nyse_all/nyse_json/part-*.json.gz',
+        f'{tgt_dir}/part-*.json.gz',
         orient = 'records',
         lines=True,
         compression='gzip',
         name_function=lambda i: '%05d' % i
     )
+    print(f'{df.compute().shape}')
 if __name__=='__main__':
     convert_file_to_json()
