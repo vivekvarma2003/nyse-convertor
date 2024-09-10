5f1b3574feade13e9ea7dd729aa2a074f74fd246
vivekvarma2003 <vv674717@gmail.com>
2024-09-10 12:30:06 +0530

    core logic implemented

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
-def hw():
-    print("Hello World")
-    return
-print(__name__)
+from dask import dataframe as dd
+columns = ['ticker', 'trade_date', 'open_price', 'low_price', 'high_price', 'close_price', 'volume']
+
+
+def convert_file_to_json():
+    df = dd.read_csv(
+        'data/nyse_all/nyse_data/*',
+        names=columns,
+        blocksize=None
+        )   
+    df.to_json(
+        'data/nyse_all/nyse_json/part-*.json.gz',
+        orient = 'records',
+        lines=True,
+        compression='gzip',
+        name_function=lambda i: '%05d' % i
+    )
 if __name__=='__main__':
-    hw()
+    convert_file_to_json()
